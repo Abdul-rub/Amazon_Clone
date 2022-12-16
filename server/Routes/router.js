7ff92faff0ecec4c1400f75a630ba54007a450c6
Abdul Rub <arub0419@gmail.com>
2022-12-16 23:09:16 +0530

    single-prod-route

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -3,12 +3,28 @@ const router = new express.Router()
 
 const Products = require("../Models/productSchema")
 
+
+//GET PRODUCTS
 router.get("/products", async(req,res)=>{
     try {
         const productsdata = await Products.find()
         // console.log(productsdata)
         res.status(201).json(productsdata)
     } catch (error) {
+        console.log("error" + error.message)
+    }
+})
+
+//GET SINGLE PRODUCTS BY ID
+router.get("/products/:id", async(req,res)=>{
+    try {
+        const {id} =req.params;
+        const singleData = await Products.findOne({id:id})
+
+        // console.log(singleData)
+        res.status(201).json(singleData)
+    } catch (error) {
+        res.status(400).json(singleData)
         console.log("error" + error.message)
     }
 })
